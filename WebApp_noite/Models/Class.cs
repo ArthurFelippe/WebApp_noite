203057cd96987c24f3cb14d64c7f36dd0291737e
ArthurFelippe <lippemedeiros321@hotmail.com>
2023-04-28 21:59:32 -0300

    Almost done

---
+++ WebApp_noite/Models/Class.cs
@@ -0,0 +1,6 @@
+﻿namespace WebApp_noite.Models
+{
+    public class Class
+    {
+    }
+}
---
+++ WebApp_noite/Models/ClienteModel.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApp_noite.Models
+{
+    public class ClienteModel
+    {
+
+        public int Id { get; set; }
+
+        public string Nome { get; set; }    
+
+        public string Telefone { get; set; }
+
+    }
+}
